d25798ae8f3ec208f85778d31d8774ea804b0fc8
Clement Corbin <corbin.clement@gmail.com>
2019-08-04 13:24:29 +0200

    use beta & gamma

--- ther.js
+++ ther.js
@@ -1,4 +1,4 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
 function handleOrientation(event) {
-  document.querySelector("pre").innerHTML += JSON.stringify(event);
+  document.querySelector("pre").innerHTML = JSON.stringify(event.beta);
 }
