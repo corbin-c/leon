da8e92dc48ea28f21c8433e51bf8f56dcb841a38
tougodo <corbin.clement@gmail.com>
2019-09-07 18:00:45 +0200

    fixed relative urls

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", async (event) => {
   event.waitUntil(caches.open("leon")
     .then((e) => e.addAll(["colortools.js","leon.js","index.html","favicon.ico",
-      "icons/32.png","icons/192.png","icons/512.png","manifest.json",
+      "./icons/32.png","./icons/192.png","./icons/512.png","manifest.json",
       "style.css","./"])));
   console.info("Service Worker initialized");
 });
