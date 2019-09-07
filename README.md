aaa940834d3a297ae9152ef063b8e6f716832b5b
tougodo <corbin.clement@gmail.com>
2019-09-07 15:33:39 +0200

    pwa

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # leon
 
-Leon got is name from famous musician & inventor Leon Theremin. This web-app provides a small "musical instrument" based on the device orientation.
+Leon got is name from famous musician & inventor Leon Theremin. This web-app
+provides a small "musical instrument" based on the device orientation.
+
+All credits go to Mickaël Espariat for this wonderful logo.
---
+++ service_worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", async (event) => {
+  event.waitUntil(caches.open("leon")
+    .then((e) => e.addAll(["colortools.js","ther.js","index.html","favicon.ico",
+      "icons/32.png","icons/192.png","icons/512.png"])));
+  console.info("Service Worker initialized");
+});
+self.addEventListener("activate", (event) => {
+  console.info("Service Worker active");
+});
+self.addEventListener("fetch", function(event) {
+  event.respondWith(caches.match(event.request)
+    .then(function(response) {
+    if (typeof response !== "undefined") {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        return response; })
+    }
+  }));
+});
