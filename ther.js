fdc776f91de9e016aaec0dda7daadde2efb2b87f
tougodo <corbin.clement@gmail.com>
2019-09-07 15:06:59 +0200

    more concise

--- ther.js
+++ ther.js
@@ -2,25 +2,13 @@ import { colortools } from "./colortools.js";
 const A_REFERENCE = 440;
 function waveToColor(frequency,intensity) {
   let pitch = 49+Math.log(frequency/A_REFERENCE)/Math.log(Math.pow(2,1/12));
-  //pitch is given relatively to A440 reference, ie. 49th note on keyboard  
   pitch = [Math.abs(pitch % 12),Math.floor(pitch/12)];
   let out_color = colortools.HSVtoRGB(pitch[0]/12,pitch[1]/4,intensity);
   document.querySelector("body").setAttribute(
     "style","background-color: "+colortools.toCSS(out_color)+";");
 }
-function WebAudio() {
-  this.type = "osc";
-  this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  this.stream = {};
-  this.audioSourceNode = {};
-  this.analyserNode = {};
-}
-function toFreq(angle) {
-  return A_REFERENCE*Math.exp(0.01*angle);
-}
-function toIntensity(angle) {
-  return 2*Math.sin(angle*(Math.PI/180)/2)+0.1;
-}
+function toFreq(angle) { return A_REFERENCE*Math.exp(0.01*angle); }
+function toIntensity(angle) { return 2*Math.sin(angle*(Math.PI/180)/2)+0.1; }
 function handleOrientation(event) {
   let f = toFreq(event.gamma);
   let i = toIntensity(event.alpha);
@@ -28,7 +16,13 @@ function handleOrientation(event) {
   gainNode.gain.setValueAtTime(i, wa.audioCtx.currentTime);
   waveToColor(f,i);
 }
-let wa = new WebAudio();
+let wa = {
+  this.type = "osc";
+  this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  this.stream = {};
+  this.audioSourceNode = {};
+  this.analyserNode = {};
+};
 let gainNode = wa.audioCtx.createGain();
 wa.audioSourceNode = wa.audioCtx.createOscillator();
 wa.audioSourceNode.type = "sine";
